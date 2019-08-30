15165e3766a489f0926d07ea4292d5e901a25acb
Tilmann Meyer <allescrafterx@gmail.com>
2019-08-30 20:40:14 +0200

    bug: test release

---
+++ release.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/changelog',
+      {
+        changelogTitle:
+          '# Changelog\n\n###This uses [Conventional Commits](https://conventionalcommits.org) for commit guidelines.',
+      },
+    ],
+    [
+      '@semantic-release/npm',
+      {
+        tarballDir: 'dist',
+      },
+    ],
+    [
+      '@semantic-release/github',
+      {
+        assets: 'dist/*.tgz',
+      },
+    ],
+
+    '@semantic-release/git',
+  ],
+};
